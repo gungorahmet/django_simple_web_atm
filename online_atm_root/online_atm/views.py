52c4dfa189c655071a2678dd831b77ce0ce01c78
afdagli <afdagli@gmail.com>
2019-10-12 23:34:30 +0300

    Added input check and error page

--- online_atm_root/online_atm/views.py
+++ online_atm_root/online_atm/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
 
+
 def homepage(request):
     return render(request, 'home.html')
+
 
 def calculate(request):
     # Get amount value from request.
     amount = request.GET['amount']
-    amount = int(amount)
+    try:
+        amount_tmp = int(amount)
+    except ValueError:
+        return render(request, 'error_page.html', {'error_message': "Money should be integer"})
+
     # We already know about $ cash types. We can hold into tuples. It's stable
-    t_cash_moneys = (100, 50, 20 , 10, 5, 2, 1)
+    t_cash_moneys = (100, 50, 20, 10, 5, 2, 1)
 
     # This will hold count of money types as dictionary.
     d_calculated_cash = {}
 
     for money in t_cash_moneys:
-        d_calculated_cash[money] = int(amount / money)
-        amount %= money
+        d_calculated_cash[money] = int(amount_tmp / money)
+        amount_tmp %= money
 
-    return render(request, 'atm.html', {'calculated_cash': d_calculated_cash.items(), 'sum_of_min_total':sum(d_calculated_cash.values())})
+    return render(request, 'atm.html', {'calculated_cash': d_calculated_cash.items(),
+                                        'sum_of_min_total': sum(d_calculated_cash.values()),
+                                        'amount': amount})
